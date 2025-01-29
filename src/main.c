4d5f86192e1ed5d67331546b6ef53fc75ce375d5
VitorPereiraFontes <vitor.fontes002@gmail.com>
2025-01-29 18:58:43 -0300

    Inicializando a matriz de LED's no arquivo main.c
    Definindo o pino para a comunicação com a matriz.
    
    Definindo as variáveis necessárias para o funcionamento do código.
    
    Chamando na função main a função de inicialização da matriz e a função
    que irá imprimir o número zero.

--- src/main.c
+++ src/main.c
@@ -4,6 +4,13 @@
 #include "hardware/pio.h"
 
 #define RED_LED_PIN 13
+// Pino que realizará a comunicação do microcontrolador com a matriz
+#define LED_MATRIX_PIN 7
+
+// Variáveis para a manipulação da matriz de LED's
+PIO pio = pio0;
+uint sm;
+volatile uint counter = 0; // Armazena a informação do número atual do display
 
 int main()
 {
@@ -13,6 +20,12 @@ int main()
     // Configurando a GPIO do LED
     gpio_init(RED_LED_PIN);
     gpio_set_dir(RED_LED_PIN,GPIO_OUT);
+
+    // Inicializando e configurando a matriz de LED's
+    sm = initialize_matrix(pio,LED_MATRIX_PIN);
+
+    // Inicializa a matriz de LED's com o número zero
+    draw_on_matrix(*(get_number_layout(counter)), pio, sm);
 
     while (true) {
         // Piscando o led
