b4b43a0bd2434437641aacb41999b99efd41511f
VitorPereiraFontes <vitor.fontes002@gmail.com>
2025-01-29 19:04:40 -0300

    Adicionando variáveis e definições para o funcionamento das interrupções
    Definindo os pinos dos botões.
    
    Declarando variáveis necessárias para debouncing via software.

--- src/main.c
+++ src/main.c
@@ -2,15 +2,23 @@
 #include "pico/stdlib.h"
 #include "matriz_leds.h"
 #include "hardware/pio.h"
+#include "interruption.h"
 
 #define RED_LED_PIN 13
 // Pino que realizará a comunicação do microcontrolador com a matriz
 #define LED_MATRIX_PIN 7
+#define BUTTON_A_PIN 5
+#define BUTTON_B_PIN 6
+// Define o intervalo de espera para que não ocorra bouncing nos botões (em microssegundos)
+#define interval 300000 // 300 ms
 
 // Variáveis para a manipulação da matriz de LED's
 PIO pio = pio0;
 uint sm;
 volatile uint counter = 0; // Armazena a informação do número atual do display
+
+// Variáveis para o controle da interrupção
+volatile uint last_time = 0; // Armazena o tempo do último evento (em microssegundos)   
 
 int main()
 {
